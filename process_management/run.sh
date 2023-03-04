68bcfdd1e8751eabef7a8c659c5223651c51a65d
Felix <felix.mars@outlook.com>
2023-03-04 21:50:53 +0000

    --

--- process_management/run.sh
+++ process_management/run.sh
@@ -2,4 +2,10 @@ echo "Running App Process Management"
 echo "Your API key is: $SUPERVISOR_TOKEN"
 echo "Your API key is: $HASSIO_TOKEN"
 
+printenv
+
+CONFIG_PATH=/data/options.json
+
+TARGET="$(bashio::config 'target')"
+
 python3 app/app.py
